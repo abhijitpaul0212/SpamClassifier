b426a33d7e6038dd02dd812a7e395b1c29c8b24e
abhijitpaul0212 <abhijitpaul0212@gmail.com>
2024-01-11 22:50:54 +0530

    fifth commit

--- src/SpamClassifier/utils/transformer.py
+++ src/SpamClassifier/utils/transformer.py
@@ -14,21 +14,18 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 class DataCleanser(BaseEstimator, TransformerMixin):
     def __init__(self):
-        try:
-            stopwords.words('english')
-        except LookupError:
-            # Specify the path where NLTK should download resources
-            cwd = os.getcwd()
-
-            # Specify the path within the root directory
-            nltk_data_path = os.path.join(cwd, 'nltk_data')
-
-            # Create the directory if it doesn't exist
-            if not os.path.exists(nltk_data_path):
-                os.makedirs(nltk_data_path)
-                
-            nltk.data.path.append(nltk_data_path)
-            nltk.download('stopwords')
+        self.stopwords = ['a', 'about', 'above', 'after', 'again', 'against', 'ain', 'all', 'am', 'an', 'and', 'any', 'are', 'aren',
+         "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', 'can', 'couldn',
+         "couldn't", 'd', 'did', 'didn', "didn't", 'do', 'does', 'doesn', "doesn't", 'doing', 'don', "don't", 'down', 'during', 'each',
+         'few', 'for', 'from', 'further', 'had', 'hadn', "hadn't", 'has', 'hasn', "hasn't", 'have', 'haven', "haven't", 'having', 'he',
+         'her', 'here', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in', 'into', 'is', 'isn', "isn't", 'it', "it's",
+         'its', 'itself', 'just', 'll', 'm', 'ma', 'me', 'mightn', "mightn't", 'more', 'most', 'mustn', "mustn't", 'my', 'myself', 'needn',
+         "needn't", 'no', 'nor', 'not', 'now', 'o', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'our', 'ours', 'ourselves', 'out',
+         'over', 'own', 're', 's', 'same', 'shan', "shan't", 'she', "she's", 'should', "should've", 'shouldn', "shouldn't", 'so', 'some',
+         'such', 't', 'than', 'that', "that'll", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', 'these', 'they', 'this',
+         'those', 'through', 'to', 'too', 'under', 'until', 'up', 've', 'very', 'was', 'wasn', "wasn't", 'we', 'were', 'weren', "weren't",
+         'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'won', "won't", 'wouldn', "wouldn't", 'y', 'you',
+         "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves']
 
     def fit(self, X, y=None):
         return self
@@ -42,7 +39,7 @@ class DataCleanser(BaseEstimator, TransformerMixin):
             review = review.lower()
             review = review.split()
 
-            review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+            review = [ps.stem(word) for word in review if not word in self.stopwords]
             review = " ".join(review)
             corpus.append(review)
         return corpus
